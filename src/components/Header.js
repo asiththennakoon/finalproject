b46550e4a4be98d05356e8f8f60426f1e022689a
asiththennakoon <asiththennakoon@gmail.com>
2023-05-11 14:19:21 +0400

    2nd commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <img src="YourLogo.png" alt="logo" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="LinkToPage1">Page 1</a></li>
+        <li><a href="LinkToPage2">Page 2</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
