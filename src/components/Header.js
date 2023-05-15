80268ba16d6e87dad99c3ac0842aba00e6350f1f
asiththennakoon <asiththennakoon@gmail.com>
2023-05-15 13:30:05 +0400

    styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import image2 from '../images/restauranfood.jpg';
 import '../styles/Header.css';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
   return (
@@ -11,7 +12,11 @@ function Header() {
                 <h1>Little Lemon</h1>
                 <h2>Chicago</h2>
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                <button>Reserve a Table</button>
+                <button>
+                  <NavLink to="/booking" end>
+                  Booking
+                  </NavLink>
+                 </button>
             </div>
             <div className='section1div1'>
                 <img src={image2} alt="Greeksalad" />
